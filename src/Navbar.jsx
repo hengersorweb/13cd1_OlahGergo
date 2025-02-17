f56d96e455739f57e95584583d607e8e405bfcc8
OlaGergo <olager397@hengersor.hu>
2025-02-17 11:58:15 +0100

    Router beállítása
    Menu rendszer kialakÍtása.

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,11 +1,17 @@
 import React from "react";
+import { Link, NavLink } from "react-router-dom";
 
-const Navbar = (()=>{
 
-    return(
+const Navbar = (() => {
+
+    return (
         <div>
-            <h1>Menu</h1>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam, esse unde accusamus ut doloremque itaque. Qui unde animi fugit asperiores enim ullam, ea dignissimos labore. Modi recusandae quidem reprehenderit laborum aut tempora sint porro? Quasi, dolorum nam fugit nemo, vel qui modi nostrum quae consectetur debitis architecto esse vitae magnam dolor, error eos perspiciatis quaerat. Provident, esse mollitia assumenda tempora ipsam odit itaque non exercitationem voluptates fuga totam eius veritatis facere fugit? Quia facere, reprehenderit similique quaerat harum molestiae voluptate commodi nam repellat accusamus, aliquam ullam id. Porro atque amet ab aspernatur, molestias, consectetur nam nihil fuga doloremque repellat earum.</p>
+            <nav>
+                <Link to='/'>Főoldal</Link>
+                <NavLink to='/ermesek'>Az olimpia érmesei</NavLink>
+                <NavLink to='/mermesek'>A magyar érmesek</NavLink>
+            </nav>
+
         </div>
     )
 
