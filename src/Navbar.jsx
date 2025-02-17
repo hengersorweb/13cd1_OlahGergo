435e6553d58bb261ee4ac5f2c3fac6aa49d019e8
OlaGergo <olager397@hengersor.hu>
2025-02-17 12:13:32 +0100

    Navbar kinézet megcsinálása

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -6,10 +6,10 @@ const Navbar = (() => {
 
     return (
         <div>
-            <nav>
-                <Link to='/'>Főoldal</Link>
-                <NavLink to='/ermesek'>Az olimpia érmesei</NavLink>
-                <NavLink to='/mermesek'>A magyar érmesek</NavLink>
+            <nav className="flex justify-between bb b--black-10">
+                <Link className="f6 link dib black dim mr3 mr4-ns" to='/'>Főoldal</Link>
+                <NavLink className="f6 link dib black dim mr3 mr4-ns" to='/ermesek'>Az olimpia érmesei</NavLink>
+                <NavLink className="f6 link dib black dim mr3 mr4-ns" to='/mermesek'>A magyar érmesek</NavLink>
             </nav>
 
         </div>
