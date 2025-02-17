663962ee0c6112dec3bf55281fa62bb745032a1c
OlaGergo <olager397@hengersor.hu>
2025-02-17 11:29:11 +0100

    Szükséges modulok importálása
    bootstrap, router, tachyons

---
+++ src/oldalak/Ermesek.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Ermesek = (()=>{
+
+    return(
+        <div>
+            <h1>Az olimpia érmesei</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam, esse unde accusamus ut doloremque itaque. Qui unde animi fugit asperiores enim ullam, ea dignissimos labore. Modi recusandae quidem reprehenderit laborum aut tempora sint porro? Quasi, dolorum nam fugit nemo, vel qui modi nostrum quae consectetur debitis architecto esse vitae magnam dolor, error eos perspiciatis quaerat. Provident, esse mollitia assumenda tempora ipsam odit itaque non exercitationem voluptates fuga totam eius veritatis facere fugit? Quia facere, reprehenderit similique quaerat harum molestiae voluptate commodi nam repellat accusamus, aliquam ullam id. Porro atque amet ab aspernatur, molestias, consectetur nam nihil fuga doloremque repellat earum.</p>
+        </div>
+    )
+
+}
+)
+export default Ermesek
